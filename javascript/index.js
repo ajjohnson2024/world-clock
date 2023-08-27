00a6cc53ca26a109a77c9043ba1b2d80fb578552
ajjohnson2024 <139026871+ajjohnson2024@users.noreply.github.com>
2023-08-27 02:14:45 -0400

    Added Javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  // Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
+  // Greece
+  let greeceElement = document.querySelector("#greece");
+  let greeceDateElement = greeceElement.querySelector(".date");
+  let greeceTimeElement = greeceElement.querySelector(".time");
+  let greeceTime = moment().tz("Europe/Athens");
+  greeceDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  greeceTimeElement.innerHTML = greeceTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
